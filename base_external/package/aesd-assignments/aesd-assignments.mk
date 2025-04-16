ba3d524431175de6eee814db163bb04e5e1e11d1
Lim Jia Zhi <34474831+JiaZhi96@users.noreply.github.com>
2025-04-16 21:32:25 +0800

    Added password and dropbear for ssh

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = f7f98e8f6177af3090895c2fd46bb973983874f9
+AESD_ASSIGNMENTS_VERSION = 39e7cb26609864ac659742a6ea90256a1cee94a5
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
